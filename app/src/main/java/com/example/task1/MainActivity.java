1996f9ddb57fbdeb6a1d487564d2370d42396998
vjmarcus <vjmarcus@list.ru>
2020-07-29 12:43:38 +0300

    Add DrawerLayout

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,43 +1,99 @@
 package com.example.task1;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.task1.model.DefaultFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
+
     private Button goToTextWatcherFragment, goToRecyclerFragment;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        goToTextWatcherFragment = findViewById(R.id.buttonGoToTextWatcherFragment);
-        goToRecyclerFragment = findViewById(R.id.buttonGoToRecyclerFragment);
+        initButtons();
+        initDrawer();
+        loadDefaultFragment();
+
         goToTextWatcherFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                TextWatcherFragment textWatcherFragment = new TextWatcherFragment();
-                transaction.replace(R.id.container, textWatcherFragment).addToBackStack(null)
-                        .commit();
+                replaceFragment(new TextWatcherFragment());
             }
         });
         goToRecyclerFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RecyclerFragment recyclerFragment = new RecyclerFragment();
-                FragmentManager manager = getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                        transaction.replace(R.id.container, recyclerFragment).commit();
+                replaceFragment(new RecyclerFragment());
             }
         });
+    }
 
+    private void loadDefaultFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.add(R.id.container, new DefaultFragment()).commit();
+    }
 
+    private void initDrawer() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, fragment).commit();
+    }
+    private void initButtons() {
+        goToTextWatcherFragment = findViewById(R.id.buttonGoToTextWatcherFragment);
+        goToRecyclerFragment = findViewById(R.id.buttonGoToRecyclerFragment);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        switch (item.getItemId()){
+            case R.id.item1:
+                replaceFragment(new DefaultFragment());
+                break;
+            case R.id.item2:
+                replaceFragment(new TextWatcherFragment());
+                break;
+            case R.id.item3:
+                replaceFragment(new RecyclerFragment());
+                break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/task1/model/DefaultFragment.java
@@ -0,0 +1,31 @@
+package com.example.task1.model;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.task1.R;
+
+
+public class DefaultFragment extends Fragment {
+
+    public DefaultFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_default, container, false);
+        return root;
+    }
+}
